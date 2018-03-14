b5331a7837673329737e328f3ff8030be1d509fd
David Lebée <DavidLebee@roxboro.biz>
2018-03-14 18:23:23 -0500

    refactoring.

--- PoweredSoft.DynamicLinq/Fluent/OrderBy/OrderByBuilder.cs
+++ PoweredSoft.DynamicLinq/Fluent/OrderBy/OrderByBuilder.cs
@@ -1,74 +1,12 @@
 ﻿using PoweredSoft.DynamicLinq.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace PoweredSoft.DynamicLinq.Fluent
 {
-    public class SortBuilderBase
-    {
-        public List<OrderByPart> Sorts { get; protected set; } = new List<OrderByPart>();
 
-        public virtual SortBuilderBase Sort(string path, QuerySortDirection sortDirection, bool appendSort)
-        {
-            Sorts.Add(new OrderByPart
-            {
-                Path = path,
-                sortDirection = sortDirection,
-                AppendSort = appendSort
-            });
-            return this;
-        }
-
-        public virtual SortBuilderBase OrderBy(string path)
-        {
-            Sorts.Clear();
-            Sorts.Add(new OrderByPart
-            {
-                Path = path,
-                sortDirection = QuerySortDirection.Ascending,
-                AppendSort = false
-            });
-            return this;
-        }
-
-        public virtual SortBuilderBase OrderByDescending(string path)
-        {
-            Sorts.Clear();
-            Sorts.Add(new OrderByPart
-            {
-                Path = path,
-                sortDirection = QuerySortDirection.Descending,
-                AppendSort = false
-            });
-            return this;
-        }
-
-        public virtual SortBuilderBase ThenBy(string path)
-        {
-            Sorts.Add(new OrderByPart
-            {
-                Path = path,
-                sortDirection = QuerySortDirection.Ascending,
-                AppendSort = true
-            });
-            return this;
-        }
-
-        public virtual SortBuilderBase ThenByDescending(string path)
-        {
-            Sorts.Add(new OrderByPart
-            {
-                Path = path,
-                sortDirection = QuerySortDirection.Descending,
-                AppendSort = true
-            });
-            return this;
-        }
-    }
-
-    public class OrderByBuilder<T> : SortBuilderBase
+    public class OrderByBuilder<T> : OrderByBuilderBase
     {
         public IQueryable<T> Query { get; }
 
