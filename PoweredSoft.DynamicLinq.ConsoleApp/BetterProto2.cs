a8661af93b6e60502523451095d5bfcef394b625
David Lebee <david@poweredsoft.com>
2018-04-10 18:26:44 -0500

    make sure its latest.

--- PoweredSoft.DynamicLinq.ConsoleApp/BetterProto2.cs
+++ PoweredSoft.DynamicLinq.ConsoleApp/BetterProto2.cs
@@ -138,6 +138,8 @@ namespace PoweredSoft.DynamicLinq.ConsoleApp
                         parentExpression, currentExpressionLambda);
                 }
             });
+
+            Result = currentExpression;
         }
     }
 }
